dcd27aaaf4e38795a0720dc68417821fc9eabe99
ThinhPhat1197 <122278694+ThinhPhat1197@users.noreply.github.com>
2026-08-16 12:19:40 +0700

    Add files via upload

---
+++ BT Buổi 6.sql
@@ -0,0 +1,33 @@
+AGGREGATE FUNCTION
+
+1. Hiển thị mỗi giao dịch thanh toán với tổng doanh thu của nhân viên đó.
+2. Hiển thị mỗi khách hàng với tổng số khách hàng trong cùng cửa hàng.
+3. Hiển thị số khách hàng tích lũy theo ngày đăng ký.
+4. Hiển thị số lần thuê lũy tiến của từng khách hàng.
+
+RANKING FUNCTION
+
+RANK()
+1. Xếp hạng phim theo giá thuê.
+2. Xếp hạng khách hàng theo tổng chi tiêu.
+3. Xếp hạng phim theo độ dài trong từng rating.
+
+ROW_NUMBER()
+1. Đánh số thứ tự giao dịch của mỗi khách hàng.
+2. Lấy 5 bộ phim đầu tiên của mỗi rating (theo alphabet).
+3. Đánh số thứ tự lần thuê của mỗi khách hàng.
+
+DENSE_RANK()
+1. Xếp hạng liên tục phim theo giá thay thế.
+2. Xếp hạng liên tục khách hàng theo số lần thuê.
+3. Xếp hạng phim theo độ dài (liên tục).
+
+LAG + LEAD
+1. So sánh doanh thu mỗi ngày với ngày hôm trước.
+2. So sánh giá thanh toán với giao dịch trước đó của khách.
+3. Xem doanh thu tháng tiếp theo là bao nhiêu.
+4. So sánh số lần thuê hôm nay với hôm qua và ngày mai.
+
+PIVOT
+1. xem số lượng phim theo từng rating (dạng bảng ngang)
+2. xem tổng doanh thu từng tháng (dạng cột ngang)
